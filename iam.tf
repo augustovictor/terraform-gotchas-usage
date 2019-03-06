73d9396990475178a03bb617418adb12ca66725d
Victor Augusto <victoraweb@gmail.com>
2019-03-06 11:10:25 -0300

    Add output with iam_users

--- iam.tf
+++ iam.tf
@@ -5,7 +5,6 @@ module "iam_user" {
   iam_policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
 }
 
-//module "iam_policy" {
-//  source = "../terraform-modules/iam/iam_policy"
-//
-//}
+output "iam_users" {
+  value = ["${module.iam_user.all_arn_iam_user}"]
+}
